9cf2e3888af71264cd2df2ca0909376c82f3525f
Kevin Templin <ktemplin91@users.noreply.github.com>
2017-10-09 13:40:29 -0500

    Added loginviewmodel and started setting up OWIN
    1. added LoginViewModel
    2. added all the OWIN NuGet packages
    3. added startup.cs and got started on the OWIN for login.

---
+++ StarcraftLadderRankings/StarcraftLadderRankings/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(StarcraftLadderRankings.Startup))]
+
+namespace StarcraftLadderRankings
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            //Enable the application to use a cookie to stroe information for the signed in user
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/LogOn")
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+        }
+    }
+}
